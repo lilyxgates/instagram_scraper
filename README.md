41ad21239ac1b92bd886d00f4bcca9d7d73789e0
Lily Gates <>
2025-12-22 15:20:32 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Instagram Scraper
+**Written by Lily Gates** 
+
+## Description
+TBD
+
+## Usage
+TBD
+
+## Required Dependencies
+TBD
+
+## Future Seteps
+TBD
---
+++ instagram_scraper.py
@@ -0,0 +1,19 @@
+# ============================================
+# Instagram Scraper
+# Author: Lily Gates
+# Created: 12/22/2025
+# Last Updated 12/22/2025
+
+# Description:
+# TBD
+
+# Features:
+# - TBD
+# ============================================
+
+import os
+import time
+import pandas as pd
+from bs4 import BeautifulSoup
+from requests import get
+from urllib.parse import urljoin
